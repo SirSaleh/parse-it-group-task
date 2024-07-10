ce4788d0872b3ef5b85db5720e7b357afca8237a
SirSaleh <animatorsaleh@gmail.com>
2024-07-11 00:44:02 +0330

    Use get_db_search_or_empty in controllers

--- rag_app/controllers/rag_controller.py
+++ rag_app/controllers/rag_controller.py
@@ -1,6 +1,7 @@
 from rag_app.services.factories.db_factory import DatabaseFactory
 from rag_app.services.nlp.nlp_model import NLPModel
 from rag_app.config.settings import settings
+from rag_app.utils.controller_utils import get_db_search_or_empty
 from typing import List
 
 class RAGController:
@@ -10,6 +11,6 @@ class RAGController:
 
     def retrieve_and_generate(self, query: str, k: int):
         query_vector = self.nlp_model.encode(query)
-        text_results = self.text_db.search(query_vector, k)
+        text_results = get_db_search_or_empty(self.text_db, query_vector, k)
         response = self.nlp_model.generate_response(text_results, query)
         return response
--- rag_app/controllers/text_controller.py
+++ rag_app/controllers/text_controller.py
@@ -1,6 +1,7 @@
 # rag_app/controllers/text_controller.py
 from rag_app.services.factories.db_factory import DatabaseFactory
 from rag_app.services.nlp.nlp_model import NLPModel
+from rag_app.utils.controller_utils import get_db_search_or_empty
 from rag_app.config.settings import settings
 
 class TextController:
@@ -14,4 +15,4 @@ class TextController:
 
     def search_text_data(self, query: str, k: int):
         query_vector = self.nlp_model.encode(query)
-        return self.db.search(query_vector, k)
+        return get_db_search_or_empty(self.db, query_vector, k)
