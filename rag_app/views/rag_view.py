045b38c89099854713c15aa7e8b5a7886d9ae399
SirSaleh <animatorsaleh@gmail.com>
2024-07-14 19:38:04 +0330

    Use get method for rag/query (rest rule)
    As we don't create any resource for RAG
    query, I used get request for /rag/query
    endpoint

--- rag_app/views/rag_view.py
+++ rag_app/views/rag_view.py
@@ -6,7 +6,7 @@ from rag_app.services.implementations.faiss_db import FAISSDatabase
 router = APIRouter()
 controller = RAGController()
 
-@router.post("/query/", response_model=ResponseSchema)
-async def query_rag_system(query: QuerySchema):
-    response = controller.retrieve_and_generate(query.query, query.k)
+@router.get("/query/", response_model=ResponseSchema)
+async def query_rag_system(query: str, k: int= 1):
+    response = controller.retrieve_and_generate(query, k)
     return {"response": response}
